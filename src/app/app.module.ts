03ccbfdb987ce1a75846949d0dbcb09e548fab6d
Kenneth Ekandem <kennyekandem@gmail.com>
2021-05-10 13:51:58 +0100

    add requirements: services, components, guards, interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultComponent } from './components/layouts/default/default.component';
+import { BlankComponent } from './components/layouts/blank/blank.component';
+import { SideNavComponent } from './components/partials/side-nav/side-nav.component';
+import { WelcomeComponent } from './components/pages/welcome/welcome.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { StartComponent } from './components/pages/start/start.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultComponent,
+    BlankComponent,
+    SideNavComponent,
+    WelcomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    StartComponent
   ],
   imports: [
     BrowserModule,
